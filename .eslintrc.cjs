145f016b3127b04fd09e86b31b78da98cdad6f43
LeoC <leo.cheng92@gmail.com>
2023-02-09 18:46:25 -0500

    initial commit, create metronome component

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin: react / jsx - runtime",
+        "plugin:prettier/recommended",
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "prettier/prettier": "error"
+    }
+}
