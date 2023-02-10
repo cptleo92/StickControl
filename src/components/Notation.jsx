a4edb64d7bb82029709e1dd15a5f8f0764388ff0
LeoC <leo.cheng92@gmail.com>
2023-02-10 12:18:06 -0500

    chore: double notes and stave

--- src/components/Notation.jsx
+++ src/components/Notation.jsx
@@ -14,13 +14,18 @@ const Notation = () => {
 
     const renderer = new Renderer(ref.current, Renderer.Backends.SVG);
 
-    renderer.resize(415, 100);
+    renderer.resize(620, 100);
     const context = renderer.getContext();
 
-    const stave = new Stave(0, 0, 400);
-    stave.addClef("bass");
+    const staveMeasure1 = new Stave(0, 0, 300);
+    staveMeasure1.addClef("bass").setContext(context).draw();
 
-    stave.setContext(context).draw();
+    const staveMeasure2 = new Stave(
+      staveMeasure1.width + staveMeasure1.x,
+      0,
+      300
+    );
+    staveMeasure2.setContext(context).draw();
 
     const notes1 = [
       new StaveNote({ keys: ["c/5"], duration: "8" }).setStyle({
@@ -39,11 +44,32 @@ const Notation = () => {
       new StaveNote({ keys: ["a/4"], duration: "8" }),
     ];
 
-    const allNotes = notes1.concat(notes2);
+    const notes3 = [
+      new StaveNote({ keys: ["c/5"], duration: "8" }).setStyle({
+        fillStyle: "green",
+        strokeStyle: "green",
+      }),
+      new StaveNote({ keys: ["a/4"], duration: "8" }),
+      new StaveNote({ keys: ["c/5"], duration: "8" }),
+      new StaveNote({ keys: ["a/4"], duration: "8" }),
+    ];
 
-    const beams = [new Beam(notes1), new Beam(notes2)];
+    const notes4 = [
+      new StaveNote({ keys: ["c/5"], duration: "8" }),
+      new StaveNote({ keys: ["a/4"], duration: "8" }),
+      new StaveNote({ keys: ["c/5"], duration: "8" }),
+      new StaveNote({ keys: ["a/4"], duration: "8" }),
+    ];
 
-    Formatter.FormatAndDraw(context, stave, allNotes);
+    const beams = [
+      new Beam(notes1),
+      new Beam(notes2),
+      new Beam(notes3),
+      new Beam(notes4),
+    ];
+
+    Formatter.FormatAndDraw(context, staveMeasure1, notes1.concat(notes2));
+    Formatter.FormatAndDraw(context, staveMeasure2, notes3.concat(notes4));
 
     beams.forEach((b) => {
       b.setContext(context).draw();
@@ -56,11 +82,14 @@ const Notation = () => {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="text-center" ref={ref}></div>
-
-      <p className={`text-5xl mx-6 ${counter === 0 && "opacity-0"}`}>
+      <p
+        className={`translate-y-2 text-5xl mx-6 ${
+          counter === 0 && "opacity-0"
+        }`}
+      >
         {Math.ceil(counter / 4)}
       </p>
+      <div className="text-center" ref={ref}></div>
     </div>
   );
 };
