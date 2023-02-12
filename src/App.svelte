aa24fe44940212c8522a941c6b81e7b0811f0f22
LeoC <leo.cheng92@gmail.com>
2023-02-12 15:31:32 -0500

    refactor: rewrite everything in svelte

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Metronome from './lib/components/Metronome.svelte';
+  import Notation from './lib/components/Notation.svelte';
+  import NotationPreview from './lib/components/NotationPreview.svelte';
+
+  import {currentPattern} from './lib/store';
+</script>
+
+<div class="flex flex-col justify-center my-8 max-w-screen-md m-auto">
+  <h1 class="text-center text-5xl my-4 underline">Stick Control</h1>
+  <h2 class="text-center text-3xl my-1">for the Snare Drummer</h2>
+  <h3 class="text-center text-xl my-1">
+    <em>... for the Screen</em>
+  </h3>
+
+  <Metronome />
+
+  {#key $currentPattern}
+    <Notation />
+    <NotationPreview />
+  {/key}
+
+  <!-- <a href="https://www.flaticon.com/free-icons/drum" title="drum icons"
+    >Drum icons created by Freepik - Flaticon</a
+  > -->
+</div>
---
+++ src/lib/components/NotationLetters.svelte
@@ -0,0 +1,40 @@
+<script>
+  import {counter, reps} from '../store';
+
+  export let pattern;
+  export let preview = false;
+
+  const letters = [];
+  for (let char of pattern) {
+    if (char !== ' ') letters.push(char.toUpperCase());
+  }
+
+  console.log('rerender');
+
+  $: previewClass = $counter > ($reps - 1) * 16 ? 'opacity-25' : 'opacity-0';
+
+  $: letterClass = idx => {
+    if (preview) return 'mr-[27px]';
+    return `mr-[27px] ${
+      idx === ($counter - 1) % 16 && 'text-emerald-800 font-bold'
+    }`;
+  };
+</script>
+
+<div
+  class={`flex translate-x-[90px] ${preview && previewClass} transition-all`}
+>
+  {#each letters.slice(0, 8) as letter, idx}
+    <div class={letterClass(idx)}>
+      {letter}
+    </div>
+  {/each}
+
+  <div class="flex ml-3">
+    {#each letters.slice(8) as letter, idx}
+      <div class={letterClass(idx + 8)}>
+        {letter}
+      </div>
+    {/each}
+  </div>
+</div>
